0b5595e54abdc358956b415f8b1cdf54dae07cd0
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-04-25 01:58:42 +0200

    update folder

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 const { spawnSync } = require("child_process");
+const ora = require("ora");
 
 const projectName = process.argv[2] || "simulator-app";
 const projectPath = path.join(process.cwd(), projectName);
@@ -12,7 +13,7 @@ fs.ensureDirSync(projectPath);
 
 const subdirs = ["simulator", "w3bstream-applet", "blockchain"];
 
-const TEMPLATE_DIR_NAME = "starter";
+const TEMPLATE_DIR_NAME = "../starter";
 
 // Copy template files
 const templatePath = path.join(__dirname, TEMPLATE_DIR_NAME);
@@ -27,13 +28,27 @@ subdirs.forEach((subdir) => {
 // Change to the new project directory
 process.chdir(projectPath);
 
+const spinner = ora(
+  "Installing dependencies with npm. This may take a few minutes."
+).start();
+
 subdirs.forEach((subdir) => {
   console.log(`Installing dependencies for ${subdir}...`);
   process.chdir(path.join(projectPath, subdir));
   spawnSync("npm", ["install"], { stdio: "inherit" });
 });
 
-console.log(`\nProject ${projectName} is ready!`);
-console.log(`\nTo get started, type the following commands:`);
-console.log(`cd ${projectName}`);
+spinner.succeed("Dependencies installed!");
+
+console.log(`\n🎉  Project ${projectName} is ready!`);
+console.log(`\nTo get started with Simulator, type the following commands:`);
+console.log(`cd simulator`);
+console.log(`touch .env`);
+console.log("Add your w3bstream vars to .env file");
 console.log(`npm start`);
+
+console.log(
+  `\nTo get started with w3bstream-applet, type the following commands:`
+);
+console.log(`cd w3bstream-applet`);
+console.log(`npm run asbuild`);
