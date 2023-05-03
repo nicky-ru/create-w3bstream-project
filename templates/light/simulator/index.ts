688e4614f5af5478a04a1c0eed6c535ccba1342e
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-05-02 21:55:34 -0300

    add simulator to light template

---
+++ templates/light/simulator/index.ts
@@ -0,0 +1,30 @@
+import { Simulator } from "@w3bstream/w3bstream-http-client-simulator";
+
+import dataGenerator from "./generator";
+import config from "./config";
+
+const { PUB_ID, PUB_TOKEN, W3BSTREAM_ENDPOINT } = config;
+const EVENT_TYPE = "DATA";
+const EVENT_ID = "DATA";
+
+const simulator = new Simulator(
+  PUB_ID,
+  PUB_TOKEN,
+  EVENT_TYPE,
+  EVENT_ID,
+  W3BSTREAM_ENDPOINT
+);
+
+simulator.init();
+
+simulator.dataPointGenerator = dataGenerator;
+
+async function start() {
+  try {
+    console.log("Starting simulator");
+    simulator.powerOn(5);
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
