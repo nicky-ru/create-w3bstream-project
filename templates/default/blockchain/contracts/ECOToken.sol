01ceb268b07ad792b09f2f2c410c335e873ec7a4
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-05-03 21:52:11 -0300

    update contracts

--- templates/default/blockchain/contracts/ECOToken.sol
+++ templates/default/blockchain/contracts/ECOToken.sol
@@ -5,26 +5,28 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract ECOToken is ERC20, AccessControl {
-
-    // Create a new role identifier for the minter role
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
-    // Create a new role identifier for the burner role
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
+    modifier onlyMinter() {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _;
+    }
+
+    modifier onlyBurner() {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+        _;
+    }
+
     constructor() ERC20("EcoFriendly Token", "ECO") {
-        // Grant the contract deployer the default admin role: it will be able
-        // to grant and revoke any roles
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function mint(address to, uint256 amount) public {
-        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+    function mint(address to, uint256 amount) public onlyMinter {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) public {
-        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+    function burn(address from, uint256 amount) public onlyBurner {
         _burn(from, amount);
     }
 }
